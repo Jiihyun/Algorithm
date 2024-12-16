86140da2d74707b0de2af69e0e6e67095e053503
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2024-12-16 11:53:41 +0900

    [level 1] Title: 약수의 합, Time: 0.03 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        if (n == 0) {
+            return 0;
+        }
+        for (int i = 1; i * i <= n; i++) {
+            if (i * i == n) {
+                sum += i;
+            } else if (n % i == 0) {
+                sum += i;
+                sum += n / i;
+            }
+        }
+        return sum;
+    }
+}
