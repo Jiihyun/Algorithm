805bf5e05591a7528c819eac006100bf7ffd7e19
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-02-08 23:39:04 +0900

    [Silver IV] Title: 수 찾기, Time: 1744 ms, Memory: 186012 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        int M = sc.nextInt();
+        for (int i = 0; i < M; i++) {
+            boolean find = false;
+            int target = sc.nextInt();
+            int start = 0;
+            int end = arr.length - 1;
+            while (start <= end) {
+                int midIndex = (start + end) / 2;
+                int midValue = arr[midIndex];
+                if (target < midValue) {
+                    end = midIndex - 1;
+                } else if (target > midValue) {
+                    start = midIndex + 1;
+                } else {
+                    find = true;
+                    break;
+                }
+            }
+            if (find) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
