c3c6964e6dfc32e00f69d0e2bd1702da8a951ba5
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-02-21 21:28:39 +0900

    [Bronze I] Title: 팰린드롬수, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,28 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<String> list = new ArrayList<>();
         while (true) {
             String read = br.readLine();
-            boolean answer = true;
-
             if (read.equals("0")) {
                 break;
             }
-            for (int i = 0; i < read.length() / 2; i++) {
-                if (read.charAt(i) != read.charAt(read.length() - 1 - i)) {
-                    answer = false;
+            list.add(read);
+        }
+        String[] answer = new String[list.size()];
+        for (int l = 0; l < list.size(); l++) {
+            char[] arr = list.get(l).toCharArray();
+            int half = arr.length / 2;
+            answer[l] = "yes";
+            for (int i = 0; i < half; i++) {
+                if (arr[i] != (arr[arr.length - 1 - i])) {
+                    answer[l] = "no";
                     break;
                 }
             }
-            if (answer) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
-            }
         }
+            for (int i = 0; i < answer.length; i++) {
+                System.out.println(answer[i]);
+            }
+        
     }
 }
