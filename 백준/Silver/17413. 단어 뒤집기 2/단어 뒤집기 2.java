7da4d170fcc22757ce4b78100d6ca3989dbea7c5
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-05-20 23:55:20 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 204 ms, Memory: 21712 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,49 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        boolean isOpen = false;
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
-
         String sentence = br.readLine();
-        for (int i = 0; i < sentence.length(); i++) {
-            char ch = sentence.charAt(i);
+        char[] arr = sentence.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        boolean isOpen = false;
 
+        for (char ch : arr) {
             if (ch == '<') {
                 while (!stack.isEmpty()) {
-                    queue.add(stack.pop());
+                    sb.append(stack.pop());
                 }
-                queue.add(ch);
                 isOpen = true;
+                sb.append(ch);
             } else if (isOpen) {
-                queue.add(ch);
+                sb.append(ch);
                 if (ch == '>') {
                     isOpen = false;
                 }
             } else if (ch == ' ') {
                 while (!stack.isEmpty()) {
-                    queue.add(stack.pop());
+                    sb.append(stack.pop());
                 }
-                queue.add(' ');
+                sb.append(ch);
             } else {
                 stack.add(ch);
             }
         }
         while (!stack.isEmpty()) {
-            queue.add(stack.pop());
-        }
-        while (!queue.isEmpty()) {
-            sb.append(queue.poll());
+            sb.append(stack.pop());
         }
         System.out.println(sb);
     }
 }
+
