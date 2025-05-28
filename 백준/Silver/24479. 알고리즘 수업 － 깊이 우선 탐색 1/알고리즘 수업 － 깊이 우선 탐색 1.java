7506e7198f26ebb6690b4d32e1f1e116745fa969
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-05-28 12:28:05 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1864 ms, Memory: 96928 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static List<Integer>[] connect;
+    static boolean[] visited;
+    static int[] order;
+    static int count = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        connect = new List[N + 1];
+        visited = new boolean[N + 1];
+        order = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            connect[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            connect[s].add(e);
+            connect[e].add(s);
+        }
+        for (int i = 1; i < N + 1; i++) {
+            Collections.sort(connect[i]);
+        }
+        dfs(R);
+        for (int i = 1; i < N + 1; i++) {
+            System.out.println(order[i]);
+        }
+    }
+
+    private static void dfs(int r) {
+        visited[r] = true;
+        order[r] = count++;
+        for (int node : connect[r]) {
+            if (!visited[node]) {
+                dfs(node);
+            }
+        }
+    }
+}
