22f58d2be94535d6420a95062d102868509917c8
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-06-17 14:53:35 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 112 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -11,12 +11,12 @@ public class Main {
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
         int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            for (int j = 4; j <= n; j++) {
-                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
-            }
             System.out.println(dp[n]);
         }
     }
