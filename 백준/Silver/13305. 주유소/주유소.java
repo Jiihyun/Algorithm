3660b6adff8f7996ee94a6793da93a04b630b951
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-07-23 11:38:46 +0900

    [Silver III] Title: 주유소, Time: 304 ms, Memory: 30492 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -8,31 +8,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] distance = new int[n - 1];
-        int leftDistance = 0;
         for (int i = 0; i < n - 1; i++) {
             distance[i] = Integer.parseInt(st.nextToken());
-            leftDistance += distance[i];
         }
         st = new StringTokenizer(br.readLine());
         int[] oilPrice = new int[n];
         for (int i = 0; i < n; i++) {
             oilPrice[i] = Integer.parseInt(st.nextToken());
         }
-        int minPrice = 1000000000;
-        for (int i = 0; i < n - 1; i++) {
-            minPrice = Math.min(minPrice, oilPrice[i]);
-        }
+
+        int minPrice = oilPrice[0];
         int answer = 0;
         for (int i = 0; i < n - 1; i++) {
-            if (oilPrice[i] != minPrice) {
-                answer += distance[i] * oilPrice[i];
-                leftDistance -= distance[i];
-            } else {
-                answer += leftDistance * minPrice;
-                break;
-            }
+            minPrice = Math.min(minPrice, oilPrice[i]);
+            answer += minPrice * distance[i];
         }
         System.out.println(answer);
     }
