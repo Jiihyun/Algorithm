0c949dc6023f9f9ba37a14bdbb3d3a078b69afdd
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-07-29 15:32:20 +0900

    [Silver V] Title: 거스름돈, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        while (true) {
+            if (n % 5 == 0) {
+                sum += n / 5;
+                System.out.println(sum);
+                break;
+            } else {
+                n -= 2;
+                sum++;
+            }
+            if (n < 0) {
+                System.out.println(-1);
+                break;
+            }
+        }
+    }
+}
