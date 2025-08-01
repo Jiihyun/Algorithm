670ca83752f80dc7e42d60bf808fcbd844e1c1b2
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-08-01 12:49:45 +0900

    [Bronze II] Title: 피보나치 수, Time: 100 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1];
+        System.out.println(fibo(n));
+    }
+
+    private static int fibo(int n) {
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n-1];
+    }
+}
