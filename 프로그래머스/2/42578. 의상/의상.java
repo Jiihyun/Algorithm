4c1078cb72b0a540af3a1971ff479b7a7bd573ee
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-08-13 13:22:28 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> m = new HashMap<>(); 
+        
+        for (int i = 0; i < clothes.length; i++) {
+            m.put(clothes[i][1], m.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        for (Integer num : m.values()) {
+            answer *= (num + 1);
+        }
+        return answer - 1;
+    }
+}
