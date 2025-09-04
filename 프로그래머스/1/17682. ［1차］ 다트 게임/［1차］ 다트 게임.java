d52408d18f86c17adf09ae788b7c47ac5cff6ee2
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-09-04 15:14:40 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.10 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -3,34 +3,39 @@ import java.util.*;
 class Solution {
     public int solution(String dartResult) {
         int[] scores = new int[3];
-        char[] arr = dartResult.toCharArray();
         int num = 0;
         int idx = 0;
-
+        char[] arr = dartResult.toCharArray();
+        
         for (int i = 0; i < arr.length; i++) {
-            if (0 <= arr[i] - '0' && arr[i] - '0' <= 9) {
-                if (arr[i] == '1' && i + 1 < arr.length && arr[i+1] == '0') {
+            char ch = arr[i];
+            if (Character.isDigit(ch)) {
+                if (ch == '1' && i + 1 < arr.length && arr[i+1] == '0') {
                     num = 10;
-                    i++; 
-                } else {
-                    num = arr[i] - '0';
+                    i++;
+                } else{
+                 num = ch - '0';   
                 }
-            }
-            else if (arr[i] == 'S') {
-                scores[idx++] = (int)Math.pow(num, 1);
-            } else if (arr[i] == 'D') {
-                scores[idx++] = (int)Math.pow(num, 2);
-            } else if (arr[i] == 'T') {
-                scores[idx++] = (int)Math.pow(num, 3);
-            }
-            else if (arr[i] == '*') {
-                scores[idx-1] *= 2;
-                if (idx - 2 >= 0) scores[idx-2] *= 2;
-            } else if (arr[i] == '#') {
-                scores[idx-1] *= -1;
+            } else if (ch == 'S' || ch == 'D' || ch == 'T') {
+                if (ch == 'S') {
+                    scores[idx] = num;
+                }
+                if (ch == 'D') {
+                    scores[idx] = (int) Math.pow(num, 2);
+                }
+                if (ch == 'T') {
+                    scores[idx] = (int) Math.pow(num, 3);
+                }
+                idx++;
+            } else if (ch == '*') {
+                if (idx -2 >= 0) {
+                    scores[idx - 2] *= 2;
+                }
+                scores[idx - 1] *= 2;
+            } else if (ch == '#') {
+                scores[idx - 1] *= -1;
             }
         }
-
         return scores[0] + scores[1] + scores[2];
     }
 }
