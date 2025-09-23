7a638eebd52e38c825722b3b67625607882c3ec8
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-09-23 12:10:17 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 26.33 ms, Memory: 89.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    static Map<Character, Integer> map = new HashMap<>();
+
+    public String solution(String[] survey, int[] choices) {        
+        for (int i = 0; i < survey.length; i++) {
+            String question = survey[i];
+            int answer = choices[i];
+            if (answer < 4) {
+                map.put(question.charAt(0), map.getOrDefault(question.charAt(0), 0) + (4 - answer));
+            } else if (answer > 4) {
+                map.put(question.charAt(1), map.getOrDefault(question.charAt(1), 0) + (answer - 4));
+            }
+        }
+        String first = getType('R', 'T');
+        String second = getType('C', 'F');
+        String third = getType('J', 'M');
+        String fourth = getType('A', 'N');
+        return first+second+third+fourth;
+    }
+    
+    private String getType(char type1, char type2) {
+        int score1 = map.getOrDefault(type1, 0);
+        int score2 = map.getOrDefault(type2, 0);
+        
+        if (score1 > score2) {
+            return String.valueOf(type1);
+        } else if (score1 < score2) {
+            return String.valueOf(type2);
+        } 
+        return type1 < type2 ? String.valueOf(type1) : String.valueOf(type2);
+    }
+}
