a30e0d9b12973aba1a02a4fc4543443eec8cc171
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2025-09-29 12:12:19 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.27 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int lux = 51, luy = 51;
+        int rdx = -1, rdy = -1;
+
+        
+        for (int i = 0; i < wallpaper.length; i++) {
+            for (int j = 0; j < wallpaper[i].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i);
+                    rdy = Math.max(rdy, j);
+                }
+            }
+        }
+        int[] answer = {lux, luy, rdx + 1, rdy + 1};
+        return answer;
+    }
+}
