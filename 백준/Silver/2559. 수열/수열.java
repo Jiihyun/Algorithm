1ddb2885ae8604bd55b1ff1ed54012586d5f788d
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-01-29 19:57:34 +0900

    [Silver III] Title: 수열, Time: 1092 ms, Memory: 25880 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        List<Integer> sumList = new ArrayList<>();
+        for (int i = 0; i <= n - k; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += arr[j];
+            }
+            sumList.add(sum);
+        }
+        int max = Integer.MIN_VALUE;
+        for (Integer sum : sumList) {
+            max = Math.max(max, sum);
+        }
+        System.out.println(max);
+    }
+}
