4d64d1c18776caf5ca659cf4b3959ca2f3b30e89
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-02-02 21:44:25 +0900

    [Silver III] Title: N번째 큰 수, Time: 1980 ms, Memory: 341576 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                nums.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        nums.sort(Comparator.reverseOrder());
+        System.out.println(nums.get(n - 1));
+    }
+}
