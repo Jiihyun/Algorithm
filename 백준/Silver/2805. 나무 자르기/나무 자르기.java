57fc3201fadda67301f0f53aca7b78a43157c590
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-02-05 21:26:33 +0900

    [Silver II] Title: 나무 자르기, Time: 428 ms, Memory: 119328 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] trees = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, trees[i]);
+        }
+
+        int left = 0;
+        int right = max;
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long sum = 0;
+
+            for (int tree : trees) {
+                if (tree > mid) {
+                    sum += (tree - mid);
+                }
+            }
+            if (sum >= m) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(answer);
+    }
+}
