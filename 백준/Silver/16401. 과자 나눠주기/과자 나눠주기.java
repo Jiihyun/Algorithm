244be0948b1cbf105d6df85d685ab36631571331
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-03-05 23:54:15 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1064 ms, Memory: 124172 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int left = 1;
+        int right = arr[n - 1];
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            long count = 0;
+            for (int i = 0; i < n; i++) {
+                count += arr[i] / mid;
+            }
+
+            if (count >= m) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(answer);
+    }
+}
