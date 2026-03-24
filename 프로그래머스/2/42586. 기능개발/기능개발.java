cfc1c40ea5580b1b1da4dd2a4f00a1ee794bc56e
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-03-24 23:58:53 +0900

    [level 2] Title: 기능개발, Time: 0.19 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,30 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-         int[] days = new int[progresses.length];
-        for (int i = 0; i<progresses.length; i++) {
-            if ((100 - progresses[i]) % speeds[i] == 0) {
-                days[i] = (100 - progresses[i]) / speeds[i];
-            } else {
-                days[i] = (100 - progresses[i]) / speeds[i] + 1;
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            int days = 0;
+            while (progresses[i] < 100) {
+                progresses[i] += speeds[i];
+                days++;
             }
+            stack.add(days);
         }
-        int now = days[0];
-        int count = 0;
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < days.length; i++) {
-            if (now < days[i]) {
-                now = days[i];
-                list.add(count);
-                count = 1;
-            } else {
+
+        List<Integer> result = new ArrayList<>();
+        int current = stack.poll();
+        int count = 1;
+        while (!stack.isEmpty()) {
+            int next = stack.poll();
+            if (next <= current) {
                 count++;
+            } else {
+                result.add(count);
+                current = next;
+                count = 1;
             }
         }
-        list.add(count);
-        int[] answer = new int[list.size()];
-        for (int i = 0; i<list.size(); i++) {
-            answer[i] = list.get(i);
+        result.add(count);
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
         }
         return answer;
     }
