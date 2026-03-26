20b875f25b0eb6a1c6e870b240f2d426bace385c
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-03-26 23:59:48 +0900

    [level 2] Title: 프로세스, Time: 1.47 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,19 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int num: priorities) {
-            pq.add(num);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            q.add(new int[]{i, priorities[i]});
+            pq.add(priorities[i]);
         }
-        while(!pq.isEmpty()) {
-            for (int i = 0; i < priorities.length; i++) {
-                if (priorities[i] == pq.peek()) {
-                    pq.poll();
-                    answer++;
-                    if (i == location) {
-                        return answer;
-                    }
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            
+            if (cur[1] == pq.peek()) {
+                answer++;
+                pq.poll();
+                
+                if (cur[0] == location) {
+                    return answer;
                 }
+            } else {
+                q.add(cur);
             }
         }
         return answer;
