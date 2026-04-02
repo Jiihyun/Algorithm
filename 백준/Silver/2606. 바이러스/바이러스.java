755edeb187eaebfc3264b536d708d79f6d8b317b
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-04-02 23:56:35 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,44 +1,44 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static List<Integer>[] connect;
+
+    static List<List<Integer>> graph = new ArrayList<>();
     static boolean[] visited;
-    static int count = 0;
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(br.readLine());
-        int connectCount = Integer.parseInt(br.readLine());
-        visited = new boolean[size + 1];
-        connect = new ArrayList[size + 1];
+        int computer = Integer.parseInt(br.readLine());
+        int pair = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i < size + 1; i++) {
-            connect[i] = new ArrayList<>();
+        for (int i = 0; i <= computer; i++) {
+            graph.add(new ArrayList<>());
         }
-
-        for (int i = 0; i < connectCount; i++) {
+        for (int i = 1; i <= pair; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            connect[s].add(e);
-            connect[e].add(s);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
-        dfs(1);
-        System.out.println(count - 1);
+        visited = new boolean[computer + 1];
+        System.out.println(dfs(1) - 1);
     }
 
-    private static void dfs(int node) {
-        visited[node] = true;
-        count++;
-        for (int i : connect[node]) {
-            if (!visited[i]) {
-                dfs(i);
+    private static int dfs(int i) {
+        int count = 1;
+        visited[i] = true;
+
+        for (int next : graph.get(i)) {
+            if (!visited[next]) {
+                count += dfs(next);
             }
         }
+        return count;
     }
 }
