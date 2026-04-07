8bd024125544c5cbb0545f6abb3c0910ab5e228e
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-04-07 23:40:00 +0900

    [Silver IV] Title: 바닥 장식, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.java
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -1,18 +1,17 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static char[][] board;
     static boolean[][] visited;
-    static int n, m;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         board = new char[n][m];
         visited = new boolean[n][m];
@@ -22,32 +21,17 @@ public class Main {
                 board[i][j] = line.charAt(j);
             }
         }
-
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (!visited[i][j]) {
-                    dfs(i, j);
+                if (board[i][j] == '-' && (j == 0 || board[i][j - 1] != '-')) {
+                    count++;
+                }
+                if (board[i][j] == '|' && (i == 0 || board[i-1][j] != '|')) {
                     count++;
                 }
             }
         }
         System.out.println(count);
-    }
-
-    private static void dfs(int y, int x) {
-        visited[y][x] = true;
-        char now = board[y][x];
-
-        if (now == '-') {
-            if (x + 1 < m && board[y][x + 1] == '-' && !visited[y][x + 1]) {
-                dfs(y, x + 1);
-            }
-        }
-        if (now == '|') {
-            if (y + 1 < n && board[y + 1][x] == '|' && !visited[y + 1][x]) {
-                dfs(y + 1, x);
-            }
-        }
     }
 }
