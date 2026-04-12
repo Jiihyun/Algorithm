9c5a1d9c2f1cf316a771731065375c19c1940a7b
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-04-12 17:45:52 +0900

    [Gold V] Title: 적록색약, Time: 144 ms, Memory: 18084 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,78 +1,86 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
+
     static int n;
-    static char[][] board;
     static boolean[][] visited;
-    static int[] dy = {1, -1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+    static char[][] sameColors;
+    static char[][] colors;
 
-    public static void main(String[] args) throws IOException {
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine());
 
-        board = new char[n][n];
-        visited = new boolean[n][n];
+        sameColors = new char[n][n];
+        colors = new char[n][n];
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < n; j++) {
-                board[i][j] = line.charAt(j);
-            }
-        }
-        int count1 = 0;
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                if (!visited[y][x]) {
-                    bfs(y, x);
-                    count1++;
+                char c = line.charAt(j);
+                colors[i][j] = c;
+                if (c == 'G') {
+                    c = 'R';
                 }
+                sameColors[i][j] = c;
             }
         }
+        int count = bfs(colors);
+        int sameCount = bfs(sameColors);
+        System.out.println(count + " " + sameCount);
+    }
 
+    private static int bfs(char[][] colors) {
+        int count = 0;
         visited = new boolean[n][n];
+        Queue<Info> q = new LinkedList<>();
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'R') {
-                    board[i][j] = 'G';
-                }
-            }
-        }
-        int count2 = 0;
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                if (!visited[y][x]) {
-                    bfs(y, x);
-                    count2++;
-                }
-            }
 
+                if (!visited[i][j]) {
+                    count++;
+
+                    char base = colors[i][j];
+                    q.add(new Info(i, j, base));
+                    visited[i][j] = true;
+
+                    while (!q.isEmpty()) {
+                        Info now = q.poll();
+
+                        for (int d = 0; d < 4; d++) {
+                            int nx = now.x + dx[d];
+                            int ny = now.y + dy[d];
+
+                            if (nx >= 0 && nx < n && ny >= 0 && ny < n
+                                    && !visited[nx][ny] && colors[nx][ny] == base) {
+                                q.add(new Info(nx, ny, colors[nx][ny]));
+                                visited[nx][ny] = true;
+                            }
+                        }
+                    }
+                }
+            }
         }
-        System.out.println(count1 + " " + count2);
+        return count;
     }
 
-    private static void bfs(int y, int x) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{y, x});
-        visited[y][x] = true;
+    private static class Info {
+        int x;
+        int y;
+        char color;
 
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int ny = now[0] + dy[i];
-                int nx = now[1] + dx[i];
-
-                if (ny >= 0 && ny < n && nx >= 0 && nx < n
-                        && board[ny][nx] == board[y][x] && !visited[ny][nx]) {
-                    visited[ny][nx] = true;
-                    queue.add(new int[]{ny, nx});
-                }
-            }
+        public Info(int x, int y, char color) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
         }
     }
+
 }
