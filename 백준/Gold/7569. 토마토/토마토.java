aa2e21b545d669870ac461b9c6d4172ea9c7ac0a
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-04-13 01:09:47 +0900

    [Gold V] Title: 토마토, Time: 616 ms, Memory: 126592 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,80 +1,81 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int m, n, h;
-    static int[] dx = {1, -1, 0, 0, 0, 0};
-    static int[] dy = {0, 0, 1, -1, 0, 0};
-    static int[] dk = {0, 0, 0, 0, 1, -1};
-    static int[][][] box;
-    static Queue<int[]> queue = new LinkedList<>();
 
-    public static void main(String[] args) throws IOException {
+    static int m, n, h;
+    static Queue<int[]> q = new LinkedList<>();
+    static int[][][] box;
+
+    static int[] dx = {1, -1, 0, 0, 0, 0};
+    static int[] dy = {0, 0, -1, 1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, 1, -1};
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
         h = Integer.parseInt(st.nextToken());
 
         box = new int[h][n][m];
-        boolean riped = true;
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < n; j++) {
+
+        for (int z = 0; z < h; z++) {
+            for (int x = 0; x < n; x++) {
                 st = new StringTokenizer(br.readLine());
-                for (int k = 0; k < m; k++) {
-                    box[i][j][k] = Integer.parseInt(st.nextToken());
-                    if (box[i][j][k] == 0) {
-                        riped = false;
-                    }
-                    if (box[i][j][k] == 1) {
-                        queue.add(new int[]{i, j, k});
+                for (int y = 0; y < m; y++) {
+                    box[z][x][y] = Integer.parseInt(st.nextToken());
+                    if (box[z][x][y] == 1) {
+                        q.add(new int[]{z, x, y});
                     }
                 }
             }
         }
-        if (riped) {
-            System.out.println(0);
-            return;
-        }
-        int result = bfs();
 
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < m; k++) {
-                    if (box[i][j][k] == 0) {
+        bfs();
+
+        int answer = 0;
+
+        for (int z = 0; z < h; z++) {
+            for (int x = 0; x < n; x++) {
+                for (int y = 0; y < m; y++) {
+                    if (box[z][x][y] == 0) {
                         System.out.println(-1);
                         return;
                     }
+                    answer = Math.max(answer, box[z][x][y]);
                 }
             }
         }
-        System.out.println(result);
+
+        System.out.println(answer - 1);
     }
 
-    private static int bfs() {
-        int count = 0;
+    private static void bfs() {
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
 
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int k = now[0];
-            int y = now[1];
-            int x = now[2];
-            for (int i = 0; i < 6; i++) {
-                int nk = dk[i] + k;
-                int ny = dy[i] + y;
-                int nx = dx[i] + x;
-                if (nk >= 0 && nk < h && ny >= 0 && ny < n && nx >= 0 && nx < m && box[nk][ny][nx] == 0) {
-                    box[nk][ny][nx] = box[k][y][x] + 1;
-                    queue.add(new int[]{nk, ny, nx});
-                    count = Math.max(count, box[nk][ny][nx]);
+            int z = now[0];
+            int x = now[1];
+            int y = now[2];
+
+            for (int d = 0; d < 6; d++) {
+                int nz = z + dz[d];
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
+                if (nz >= 0 && nx >= 0 && ny >= 0
+                        && nz < h && nx < n && ny < m
+                        && box[nz][nx][ny] == 0) {
+
+                    box[nz][nx][ny] = box[z][x][y] + 1;
+                    q.add(new int[]{nz, nx, ny});
                 }
             }
         }
-        return count - 1;
     }
 }
-
