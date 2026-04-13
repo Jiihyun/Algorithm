a0d6bf4ad5d5faba06edcd3387ba4c6ccf4519dd
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-04-14 01:43:24 +0900

    [Silver IV] Title: 점화식, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static long[] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        dp = new long[n + 1];
+
+        dp[0] = 1;
+        if (n >= 1) {
+            dp[1] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
