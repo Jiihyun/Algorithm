c8b5bcd3b317af949f78afcf12ff14ad7cc64317
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-04-24 02:28:27 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,33 +1,38 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(br.readLine());
-        int[] dp = new int[size + 1];
-        int[] score = new int[size + 1];
-        for (int i = 1; i <= size; i++) {
-            score[i] = Integer.parseInt(br.readLine());
-        }
-        if (size == 1) {
-            System.out.println(score[1]);
-        } else if (size == 2) {
-            System.out.println(score[1] + score[2]);
-        } else {
-            dp[1] = score[1];
-            dp[2] = score[1] + score[2];
-            dp[3] = Math.max(score[1] + score[3], score[2] + score[3]);
+        int n = Integer.parseInt(br.readLine());
 
-            for (int i = 4; i <= size; i++) {
-                int a = dp[i - 2] + score[i];
-                int b = dp[i - 3] + score[i] + score[i - 1];
-                dp[i] = Math.max(a, b);
-            }
-            System.out.println(dp[size]);
+        int[] stair = new int[n];
+        for (int i = 0; i < n; i++) {
+            stair[i] = Integer.parseInt(br.readLine());
         }
+
+        if (n == 1) {
+            System.out.println(stair[0]);
+            return;
+        }
+
+        if (n == 2) {
+            System.out.println(stair[0] + stair[1]);
+            return;
+        }
+
+        int[] dp = new int[n];
+        dp[0] = stair[0];
+        dp[1] = stair[0] + stair[1];
+        dp[2] = Math.max(stair[0] + stair[2], stair[1] + stair[2]);
+
+        for (int i = 3; i < n; i++) {
+            dp[i] = Math.max(
+                    dp[i - 3] + stair[i - 1] + stair[i],
+                    dp[i - 2] + stair[i]
+            );
+        }
+
+        System.out.println(dp[n - 1]);
     }
 }
-
