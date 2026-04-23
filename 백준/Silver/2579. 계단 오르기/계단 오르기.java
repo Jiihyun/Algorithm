5630c30f7e63bef43dfa513a0d9c707a21f13fb9
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-04-24 02:28:47 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,38 +1,12 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int[] stair = new int[n];
-        for (int i = 0; i < n; i++) {
-            stair[i] = Integer.parseInt(br.readLine());
-        }
-
-        if (n == 1) {
-            System.out.println(stair[0]);
-            return;
-        }
-
-        if (n == 2) {
-            System.out.println(stair[0] + stair[1]);
-            return;
-        }
-
-        int[] dp = new int[n];
-        dp[0] = stair[0];
-        dp[1] = stair[0] + stair[1];
-        dp[2] = Math.max(stair[0] + stair[2], stair[1] + stair[2]);
-
-        for (int i = 3; i < n; i++) {
-            dp[i] = Math.max(
-                    dp[i - 3] + stair[i - 1] + stair[i],
-                    dp[i - 2] + stair[i]
-            );
-        }
-
-        System.out.println(dp[n - 1]);
-    }
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
