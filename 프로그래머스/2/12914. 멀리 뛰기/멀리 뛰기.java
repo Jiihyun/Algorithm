02ef298ebc9cb6837e86e7461480889ffad612bf
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-05-03 14:16:48 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,13 +1,17 @@
 class Solution {
     public long solution(int n) {
-        long[] answer = new long[2001];
-        answer[1] = 1;
-        answer[2] = 2;
- 
-        for (int i = 3; i < 2001; i++) {
-            answer[i] = (answer[i - 1] + answer[i - 2]) % 1234567;
+        if (n == 1) {
+            return 1;
         }
- 
-        return answer[n];
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+        return dp[n] % 1234567;
     }
 }
