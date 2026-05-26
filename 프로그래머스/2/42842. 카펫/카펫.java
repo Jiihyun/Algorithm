d7f6ca738bce731ebd9a93162546210d115e3f81
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-05-26 09:47:14 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,24 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
+        int ga = 0; 
+        int se = 0;
         
-        int sum = brown + yellow;
-        
-        for (int i = 3; i <= sum; i++) {
-            int y = i;
-            int x = sum / y; 
-            
-            if (y < 3) {
-                continue;
+        for (int i = 1; i <= Math.sqrt(yellow); i++) {
+            if (yellow % i == 0) {
+                se = i;
+                ga = yellow / i;
             }
             
-            if (x >= y
-               && (y-2) * (x-2) == yellow) {
-                answer[0] = x;
-                answer[1] = y;
-                break;
+            int total = (ga + 2) * (se + 2);
+
+            if (total - yellow == brown) {
+                 return new int[]{ga + 2, se + 2};
             }
         }
-        return answer;
+        return new int[]{0, 0};
     }
 }
+
+// yellow = 1 -> brown = 8 [3,3]
+// yellow = 2 -> brown = 10 [4,3] 짝
+// yellow = 3 -> brown = 12 [5,3]
+// yellow = 4 -> brown = 12 [4,4] 짝
+// yellow = 5 -> brown = 16 [7,3]
+// yellow = 6 -> brown = 14 [5,4] 짝
+// yellow = 7 -> brown = 20 [9,3]
