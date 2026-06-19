a7c214f48c56667299e95c1f927e8fcfe304af0a
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-06-19 09:52:56 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,29 @@
 import java.util.*;
 
 class Solution {
+    
     static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
         visited = new boolean[n];
-        int networkCount = 0;
+        int count = 0;
+        
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 dfs(i, computers);
-                networkCount++;
+                count++;
             }
         }
-        return networkCount;
+        return count;
     }
     
-    public void dfs(int current, int[][] computers) {
+    private void dfs(int current, int[][] computers) {
         visited[current] = true;
         for (int i = 0; i < computers.length; i++) {
-            if (!visited[i] && computers[current][i] == 1) {
+            if (computers[current][i] == 1 & !visited[i]) {
                 dfs(i, computers);
             }
         }
     }
 }
+
