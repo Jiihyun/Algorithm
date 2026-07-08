03938ae38045b70dc335ca630859644cd87ce065
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-07-08 23:04:42 +0900

    [level 2] Title: 할인 행사, Time: 35.44 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,36 +1,28 @@
 import java.util.*;
 
 class Solution {
-    static Map<String, Integer> bucket = new HashMap<>();
-
     public int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0;
-        initMap(want, number);
+        List<String> items = new ArrayList<>();
+        for (int i = 0; i < number.length; i++) {
+            for (int j = 0; j < number[i]; j++) {
+                items.add(want[i]);
+            }
+        }
         
+        int count = 0;
         for (int i = 0; i <= discount.length - 10; i++) {
-            boolean pass = false;
+            List<String> currentItems = new ArrayList<>(items);
             for (int j = i; j < i + 10; j++) {
-                String item = discount[j];
-                if (bucket.containsKey(item) && bucket.get(item) > 0) {
-                    bucket.put(item, bucket.get(item) - 1);
-                    pass = true;
-                } else {
-                    initMap(want, number);
-                    pass = false;
-                    break;
+                if (currentItems.contains(discount[j])) {
+                    currentItems.remove(discount[j]);
                 }
             }
-            if (pass) {
-                initMap(want, number);
-                answer++;
+            if (currentItems.isEmpty()) {
+                count++;
             }
         }
-        return answer;
-    }
-    
-    private void initMap(String[] want, int[] number) {
-        for (int i = 0; i < want.length; i++) {
-            bucket.put(want[i], number[i]);
-        }
+        return count;
     }
 }
+
+// 이게 완탐이 가능한 건가?? 모르게씀 100000개가 있고 원하는 건 10개인데 시간 복잡도 계ㅅ산을 어케 함?
