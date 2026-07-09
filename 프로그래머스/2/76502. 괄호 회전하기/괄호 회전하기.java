749c31a97c6f339935f909b2ef68459d328c1c76
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-07-09 23:48:29 +0900

    [level 2] Title: 괄호 회전하기, Time: 22.06 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,41 +2,39 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        String origin = s;
+        int count = 0 ;
         
         for (int i = 0; i < s.length(); i++) {
-            s = origin.substring(i) + origin.substring(0,i);
-            
-            Deque<Character> stack = new ArrayDeque<>();
-            boolean isValid = true;
-            
-            for (Character ch: s.toCharArray()) {
-                if ( ch == '[' || ch == '(' || ch == '{') {
-                    stack.push(ch);
-                    continue;
-                }
-                if (stack.isEmpty()) {
-                    isValid = false;
-                    break;
-                }
-                if (stack.peek() == '(' && ch == ')') { 
-                    stack.pop();
-                } else if (stack.peek() == '{' && ch == '}') { 
-                    stack.pop();
-                } else if (stack.peek() == '[' && ch == ']') { 
-                    stack.pop();
-                } else {
-                    isValid = false;
-                    break;
-                }
-
-            }
-            if (isValid && stack.isEmpty()) {
-                answer++;
+            String str = s.substring(i, s.length()) + s.substring(0, i);
+            if (isRight(str)) {
+                count++;
             }
         }
-        return answer;
+        return count;
+    }
+    
+    private boolean isRight(String str) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            Character ch = str.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.add(ch);
+            } else {
+                if (!st.isEmpty()) {
+                    if ((ch == ')' && st.peek() == '(')
+                       || (ch == ']' && st.peek() == '[')
+                       || (ch == '}' && st.peek() == '{')) {
+                        st.pop();
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (st.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 }
-
