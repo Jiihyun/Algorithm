686b86cee560da8f97cbbc72e64afaad15feae0f
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-07-14 23:40:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.36 ms, Memory: 57.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,41 +1,38 @@
 import java.util.*;
 
 class Solution {
-    
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
 
     public int solution(int[][] maps) {
-        int x = maps.length;
-        int y = maps[0].length;
-        boolean[][] visited = new boolean[x][y];
-        
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0,1});
-        
-        visited[0][0] = true;
-        
+
+        Deque<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0, 0});
+
         while (!q.isEmpty()) {
             int[] cur = q.poll();
+
             int cx = cur[0];
             int cy = cur[1];
-            int distance = cur[2];
-            
-            if (cx == x - 1 && cy == y - 1) {
-                return distance;
-            }
-            
+
             for (int i = 0; i < 4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-                
-                if (nx >= 0 && nx < x && ny >= 0 && ny < y
-                   && !visited[nx][ny] && maps[nx][ny] == 1) {
-                    q.add(new int[]{nx, ny, distance+1});
-                    visited[nx][ny] = true;
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx >= 0 && nx < maps.length
+                        && ny >= 0 && ny < maps[0].length
+                        && maps[nx][ny] == 1) {
+
+                    maps[nx][ny] = maps[cx][cy] + 1;
+                    q.add(new int[]{nx, ny});
                 }
             }
         }
-        return -1;
+
+        if (maps[maps.length - 1][maps[0].length - 1] == 1) {
+            return -1;
+        }
+        return maps[maps.length - 1][maps[0].length - 1];
     }
 }
