377ed41bd7a578ef6d97bcd2b4b64f56e36e2aa7
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-08-19 23:57:00 +0900

    [level 3] Title: N으로 표현, Time: 16.21 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,36 +1,37 @@
 import java.util.*;
 
 class Solution {
-
     public int solution(int N, int number) {
-        List<Set<Integer>> dp = new ArrayList<>();
-        dp.add(new HashSet<>()); 
+        Set<Integer>[] dp = new HashSet[9];
 
         for (int i = 1; i <= 8; i++) {
-            Set<Integer> set = new HashSet<>();
+            dp[i] = new HashSet<>();
+        }
 
-            // 1. 5, 55, 555 같은 수 만들기
-            int repeated = Integer.parseInt(String.valueOf(N).repeat(i));
-            set.add(repeated);
+        for (int i = 1; i <= 8; i++) {
+            int connected = 0;
+            for (int j = 0; j < i; j++) {
+                connected = connected * 10 + N;
+            }
+            dp[i].add(connected);
 
-            // 2. 이전 dp 조합
             for (int j = 1; j < i; j++) {
-                Set<Integer> left = dp.get(j);
-                Set<Integer> right = dp.get(i - j);
 
-                for (int a : left) {
-                    for (int b : right) {
-                        set.add(a + b);
-                        set.add(a - b);
-                        set.add(a * b);
+                for (int a : dp[j]) {
+                    for (int b : dp[i - j]) {
+
+                        dp[i].add(a + b);
+                        dp[i].add(a - b);
+                        dp[i].add(a * b);
+
                         if (b != 0) {
-                            set.add(a / b);
+                            dp[i].add(a / b);
                         }
                     }
                 }
             }
-            dp.add(set);
-            if (set.contains(number)) {
+
+            if (dp[i].contains(number)) {
                 return i;
             }
         }
