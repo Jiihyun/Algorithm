e6c5cfe7f072295eeceb2560695dd444c84e021e
Jihyun Lim <120218763+Jiihyun@users.noreply.github.com>
2026-09-02 09:51:05 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,37 @@
-import java.util.*;
-
 class Solution {
     
     static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
         visited = new boolean[n];
-        int count = 0;
+        int answer = 0;
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 dfs(i, computers);
-                count++;
+                answer++;
             }
         }
-        return count;
+        return answer;
     }
     
-    private void dfs(int current, int[][] computers) {
-        visited[current] = true;
-        for (int i = 0; i < computers.length; i++) {
-            if (computers[current][i] == 1 & !visited[i]) {
-                dfs(i, computers);
+    private void dfs(int i, int[][] computers) {
+        visited[i] = true;
+        
+        for (int j = 0; j < computers.length; j++) {
+            if (computers[i][j] == 1 && !visited[j]) {
+                dfs(j, computers);
             }
         }
     }
 }
 
+/*
+연결된 덩어리 개수 구하는 문제네.
+
+한 노드에서 연결된 모든 노드를 방문하면 되겠네.
+
+DFS/BFS 둘 다 되는데 DFS가 코드가 좀 간단하겠다.
+
+→ DFS 선택
+*/
